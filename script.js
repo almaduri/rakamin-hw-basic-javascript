3dfa999e03e5d21b956f7d62b132188a903905dc
almaduri <ali.mdrw@gmail.com>
2023-03-28 11:01:23 +0700

    add function thousandSeparator and manipulate the table

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const submitButton = document.querySelector("button.submit-button")
 const inputNama = document.getElementById("inputNama")
 const inputUmur = document.getElementById("inputUmur")
 const inputSangu = document.getElementById("inputSangu")
+const tbody = document.querySelector("tbody.table-group-divider")
 
 function resetAllInput() {
   inputNama.value = ""
@@ -17,9 +18,15 @@ class Pendaftar {
   }
 }
 
+function thousandSeparator(num) {
+  return num.toLocaleString().replaceAll(",", ".")
+}
+
+const listPendaftar = []
+
 submitButton.addEventListener("click", function(event) {
   event.preventDefault()
-  const form = document.querySelector('.needs-validation')
+  const form = document.querySelector(".needs-validation")
   
   if (!form.checkValidity()) {
     event.preventDefault()
@@ -27,7 +34,28 @@ submitButton.addEventListener("click", function(event) {
     form.classList.add("was-validated")
   } else {
     form.classList.remove("was-validated")
+    listPendaftar.push(new Pendaftar(inputNama.value, parseInt(inputUmur.value), parseInt(inputSangu.value)))
+    console.log(listPendaftar)
     resetAllInput()
+
+    const averageUmur = listPendaftar.reduce((a, b) => a + b.umur, 0) / listPendaftar.length
+    const averageUangSangu = listPendaftar.reduce((a, b) => a + b.uangSangu, 0) / listPendaftar.length
+    
+    const element = `
+    ${listPendaftar.map((pendaftar, index) =>
+      `<tr>
+      <th scope="row">${index + 1}</td>
+      <td>${pendaftar.nama}</td>
+      <td>${pendaftar.umur}</td>
+      <td>Rp${thousandSeparator(pendaftar.uangSangu)}</td>
+      </tr>`
+      ).join("")}
+      
+    <tr>
+      <td colspan="4">Rata rata pendaftar memiliki uang sangu sebesar Rp${thousandSeparator(averageUangSangu)} dengan rata rata umur ${averageUmur} tahun</td>
+    </tr>
+    `
+    tbody.innerHTML = element
   }
   
 })
